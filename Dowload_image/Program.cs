9f4a449ce74f8eceea0579cb2fa123c41edc769d
AndroidT50 <westfit@yandex.ru>
2022-11-28 16:59:55 +0700

    Нашел тяжелую картинку срабочей ссылкой

--- Dowload_image/Program.cs
+++ Dowload_image/Program.cs
@@ -27,7 +27,8 @@ namespace Dowload_image
         static void Main(string[] args)
         {
             ImageDownloader imageDownloader = new ImageDownloader();
-            imageDownloader.Download("https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg", "bigimage.jpg");
+            ////imageDownloader.Download("https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg", "bigimage.jpg");
+            imageDownloader.Download("https://images.hdqwalls.com/wallpapers/vossen-beautiful-wrapped-corvette-c8-8k-ag.jpg", "bigimage.jpg");
        
         }
     }
