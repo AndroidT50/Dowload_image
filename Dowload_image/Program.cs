44a2ca59b5655e589abea126fe86768cd68790d7
AndroidT50 <westfit@yandex.ru>
2022-11-28 17:18:03 +0700

    Добавил еще одну тяжелую картинку 27 мб

--- Dowload_image/Program.cs
+++ Dowload_image/Program.cs
@@ -29,7 +29,7 @@ namespace Dowload_image
             ImageDownloader imageDownloader = new ImageDownloader();
             ////imageDownloader.Download("https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg", "bigimage.jpg");
             imageDownloader.Download("https://images.hdqwalls.com/wallpapers/vossen-beautiful-wrapped-corvette-c8-8k-ag.jpg", "bigimage.jpg");
-       
+            //https://images.hdqwalls.com/wallpapers/tenet-movie-8k-a1.jpg очень тяжелая 27mb
         }
     }
 }
