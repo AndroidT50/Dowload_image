21a30e95fe071daf8bf9779817d394957bf68cc3
AndroidT50 <westfit@yandex.ru>
2022-11-30 12:07:27 +0700

    Доделаны события полностью, подправлен код

--- Dowload_image/Program.cs
+++ Dowload_image/Program.cs
@@ -16,43 +16,33 @@ namespace Dowload_image
             public string remoteUri;
             public string fileName;
 
-            public event Action<string,string> ImageStarted;
-            public event Action<string,string> ImageCompleted;
-             
-
+            public event Action <string> ImageStarted;
+            public event Action <string> ImageCompleted;
             public string Download(string remoteUri, string fileName)
             {
-                
                 var myWebClient = new WebClient();
                 ImageStarted += IStarted;
-                ImageStarted?.Invoke(remoteUri, fileName);
-                myWebClient.DownloadFile(remoteUri, fileName);
-                ImageCompleted += IsCompleted;
-                ImageCompleted?.Invoke(remoteUri, fileName);
-                return remoteUri;
-                
-
-            }
-
-            private void IsCompleted(string remoteUri, string fileName)
-            {
-                Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
-            }
-
-            private void IStarted(string remoteUri, string fileName)
-            {
+                ImageStarted?.Invoke(fileName);
                 Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+                myWebClient.DownloadFile(remoteUri, fileName);
+                Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+                ImageCompleted += IsCompleted;
+                ImageCompleted?.Invoke(fileName);
+                return remoteUri;
             }
-
+            private void IsCompleted(string fileName)
+            {
+                Console.WriteLine($"Скачивание файла {fileName} закончилось");
+            }
+            private void IStarted(string fileName)
+            {
+                Console.WriteLine($"Скачивание файла {fileName} началось ");
+            }
         }
         static void Main(string[] args)
         {
             ImageDownloader imageDownloader = new ImageDownloader();
             imageDownloader.Download("https://images.hdqwalls.com/wallpapers/tenet-movie-8k-a1.jpg", "bigimage.jpg");
-            
-            
         }
-
-       
     }
 }
