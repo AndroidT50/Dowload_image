6a23ddc7d8fafe745fc541a23605748d1ae1a40d
Vint k <vintkorp@outlook.com>
2022-12-12 23:15:21 +0700

    Итог

---
+++ Dowload_image/ImageDownloader.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System;
+
+namespace Dowload_image
+{
+    public class ImageDownloader
+    {
+        public string RemoteUri;
+        public string FileName;
+
+        public event Action<string, string> ImageStarted;
+        public event Action<string, string> ImageCompleted;
+
+        public string Download(string remoteUri, string fileName)
+        {
+            var myWebClient = new WebClient();
+            ImageStarted?.Invoke(remoteUri, fileName);
+            myWebClient.DownloadFile(remoteUri, fileName);
+            ImageCompleted?.Invoke(remoteUri, fileName);
+            return remoteUri;
+        }
+    }
+}
--- Dowload_image/Program.cs
+++ Dowload_image/Program.cs
@@ -1,48 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Runtime.Remoting.Lifetime;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dowload_image
 {
     internal class Program
     {
-        delegate string DowImage(string remoteUrl, string fileName);
-        public class ImageDownloader
-        {
-            public string remoteUri;
-            public string fileName;
-
-            public event Action <string> ImageStarted;
-            public event Action <string> ImageCompleted;
-            public string Download(string remoteUri, string fileName)
-            {
-                var myWebClient = new WebClient();
-                ImageStarted += IStarted;
-                ImageStarted?.Invoke(fileName);
-                Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
-                myWebClient.DownloadFile(remoteUri, fileName);
-                Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
-                ImageCompleted += IsCompleted;
-                ImageCompleted?.Invoke(fileName);
-                return remoteUri;
-            }
-            private void IsCompleted(string fileName)
-            {
-                Console.WriteLine($"Скачивание файла {fileName} закончилось");
-            }
-            private void IStarted(string fileName)
-            {
-                Console.WriteLine($"Скачивание файла {fileName} началось ");
-            }
-        }
         static void Main(string[] args)
         {
             ImageDownloader imageDownloader = new ImageDownloader();
+            imageDownloader.ImageStarted += IStarted;
+            imageDownloader.ImageCompleted += IsCompleted;
             imageDownloader.Download("https://images.hdqwalls.com/wallpapers/tenet-movie-8k-a1.jpg", "bigimage.jpg");
         }
+        static  void IsCompleted(string remoteUri, string fileName)
+        {
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+        }
+        static void IStarted(string remoteUri, string fileName)
+        {
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+        }
+
     }
 }
